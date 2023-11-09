45c6ccb9ba37e6ebfac774da6153f804e7ea26f5
Santhosh Reddy Gurram <santhoshreddy4148@gmail.com>
2023-11-09 14:11:52 -0600

    Assigment 12 Part 1 commit

--- controllers/Vechile.js
+++ controllers/Vechile.js
@@ -65,3 +65,15 @@ exports.Vechile_create_post = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+
+//New code for Assigment12 for screenshot1
+exports.Vechile_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await Vechile.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
